3b6369c0122e0174e41b97e3b06cf4d8598c22ff
justin !! <jzou0417a@gmail.com>
2025-12-29 19:06:15 -0500

    Refactor ObjectBootstrapper
    used gemini because i dont trust myself ( :

--- src/server/mOS_Server/Modules/ObjectBootstrapper.lua
+++ src/server/mOS_Server/Modules/ObjectBootstrapper.lua
@@ -1,5 +1,6 @@
 --[[
     sets up tagged objects and sends a request to build the object when initialization conditions meet
+    REFACTOR: used gemini to protect against leaks
 ]]
 
 local dir = require(game.ReplicatedStorage.Shared.mOS_Replicated.Directory)
@@ -8,51 +9,28 @@ local CS = game:GetService("CollectionService")
 
 local ServerSignals = dir.ServerSignals
 
-local function _checkServerPreload(required)
-    if not required:GetAttribute(dir.Consts.LAZY_LOAD_SERVER_CONTROLLER_ATTR) then
-        ServerSignals.InitObject:Fire(required)
-    end
-end
 
 local function Initialize(player, required)
-    --[[
-        this will initialize the server object only if it hasn't been created yet
-        server objects should only be used if you need something to exist beyond the
-        scope of the client that's essential to the object itself
-
-        good (should use a server object): ammo rack state thats interactible on both 
-        server/client, where the client needs to communicate to the server to specificlaly 
-        act upon that object's specific ammo rack
-
-        bad (should not use a server object): particle spawning, you can do this without
-        a connection to the server object
-
-        client object will be initialized below
-    ]]
     if player then
         dir.NetUtils:FireClient(player, dir.Events.Reliable.OnInitialize, required)
     end
-
     ServerSignals.InitObject:Fire(required)
 end
 
 local function Destroy(player, required)
-    --[[
-        server objects don't explicitly destroy themselves here
-
-        important: this means server objects are responsible for their own cleanup!!!
-        you have to set up something on your servercontroller to do that or else there
-        will be a memory leak
-
-        client object will be de-initialized below
-    ]]
     dir.NetUtils:FireClient(player, dir.Events.Reliable.OnDestroy, required)
 end
 
 local function AddSeatInitListener(required)
-    required:SetAttribute(dir.Consts.OBJECT_IDENT_ATTR, HTTP:GenerateGUID())
-    _checkServerPreload(required)
-    local seat = required.ControlSeat.Value
+    local seatValue = required:FindFirstChild("ControlSeat")
+    if not seatValue then
+        warn("Missing ControlSeat ObjectValue for:", required:GetFullName())
+        return
+    end
+
+    local seat = seatValue.Value
+    if not seat then return end
+
     local lastOccupant
     seat:GetPropertyChangedSignal("Occupant"):Connect(function()
         local occupant = seat.Occupant
@@ -64,7 +42,6 @@ local function AddSeatInitListener(required)
             end
         else
             if not lastOccupant or not lastOccupant.Parent then
-                warn("occupant missing?")
                 return
             end
             local player = game.Players:GetPlayerFromCharacter(lastOccupant)
@@ -78,11 +55,13 @@ end
 
 local function AddToolInitListener(required)
     local tool = required.Parent
-    assert(tool:IsA("Tool"), "you cant add a tool init listener to a non-tool idiot")
+
+    if not tool or not tool:IsA("Tool") then
+        return
+    end
+
     local owner = nil
-    local toolId = HTTP:GenerateGUID()
-    required:SetAttribute(dir.Consts.OBJECT_IDENT_ATTR, toolId)
-    --_checkServerPreload(required)
+
     tool.Equipped:Connect(function()
         local char = tool.Parent
         local player = game.Players:GetPlayerFromCharacter(char)
@@ -91,38 +70,74 @@ local function AddToolInitListener(required)
             Initialize(player, required)
         end
     end)
+
     tool.Unequipped:Connect(function()
         if not owner then return end
         Destroy(owner, required)
+        owner = nil
     end)
 end
 
 local function PrepImmediateSpawn(required)
-    required:SetAttribute(dir.Consts.OBJECT_IDENT_ATTR, HTTP:GenerateGUID())
+    -- just needs the ID and preload check, which happens in SpawnListener
 end
 
-local function SpawnListener(required, add)
-    add(required)
-    _checkServerPreload(required)
+
+
+local TagHandlers = {
+    [dir.Consts.SEATED_INIT_TAG_NAME] = AddSeatInitListener,
+    [dir.Consts.TOOL_INIT_TAG_NAME] = AddToolInitListener,
+    [dir.Consts.SPAWN_INIT_TAG_NAME] = PrepImmediateSpawn
+}
+
+-----------------------------------------------------------------------------------
+
+local function SpawnListener(required, handler)
+    local function execute()
+        if required:GetAttribute("_hasSpawned") then return end
+        required:SetAttribute("_hasSpawned", true)
+
+        if not required:GetAttribute(dir.Consts.OBJECT_IDENT_ATTR) then
+            required:SetAttribute(dir.Consts.OBJECT_IDENT_ATTR, HTTP:GenerateGUID())
+        end
+
+        handler(required)
+
+        -- check preload (whether server obj. only initializes itself after the client obj. is entered)
+        if not required:GetAttribute(dir.Consts.LAZY_LOAD_SERVER_CONTROLLER_ATTR) then
+            ServerSignals.InitObject:Fire(required)
+        end
+    end
+
+    if required:IsDescendantOf(workspace) then
+        execute()
+    else
+        -- wait till this is connected
+        local connection
+        local destroyConn
+        destroyConn = required.Destroying:Connect(function()
+            if connection then connection:Disconnect() end
+            if destroyConn then destroyConn:Disconnect() end
+        end)
+
+        connection = required.AncestryChanged:Connect(function(_, parent)
+            if parent and required:IsDescendantOf(workspace) then
+                connection:Disconnect()
+                if destroyConn then destroyConn:Disconnect() end
+                execute()
+            end
+        end)
+    end
 end
 
 return function()
-    for _, v in pairs(CS:GetTagged(dir.Consts.SEATED_INIT_TAG_NAME)) do
-        SpawnListener(v, AddSeatInitListener)
-    end
-    for _, v in pairs(CS:GetTagged(dir.Consts.TOOL_INIT_TAG_NAME)) do
-        SpawnListener(v, AddToolInitListener)
-    end
+    for tag, handler in pairs(TagHandlers) do
+        for _, v in pairs(CS:GetTagged(tag)) do
+            SpawnListener(v, handler)
+        end
 
-    for _, v in pairs(CS:GetTagged(dir.Consts.SPAWN_INIT_TAG_NAME)) do
-        SpawnListener(v, PrepImmediateSpawn)
+        CS:GetInstanceAddedSignal(tag):Connect(function(inst)
+            SpawnListener(inst, handler)
+        end)
     end
-
-    CS:GetInstanceAddedSignal(dir.Consts.SEATED_INIT_TAG_NAME):Connect(function(inst)
-        SpawnListener(inst, AddSeatInitListener)
-    end)
-    CS:GetInstanceAddedSignal(dir.Consts.TOOL_INIT_TAG_NAME):Connect(function(inst)
-        SpawnListener(inst, AddToolInitListener)
-    end)
-    CS:GetInstanceAddedSignal(dir.Consts.SPAWN_INIT_TAG_NAME):Connect(_checkServerPreload)
 end
