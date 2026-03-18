c987d8430bd9c6273c32d1d5e35899a685191ddd
justin <jzou0417a@gmail.com>
2026-03-18 12:15:27 -0400

    Update ObjectInitializer to v2 initroot

--- src/ReplicatedStorage/_Main/Modules/ObjectManagement/ObjectInitializer.lua
+++ src/ReplicatedStorage/_Main/Modules/ObjectManagement/ObjectInitializer.lua
@@ -1,7 +1,7 @@
 --#region required
 local dir = require(script.Parent.Parent.Parent.Directory)
-local validator = dir.Validator.new(script.Name)
 local ObjectRegistry = require(script.Parent.ObjectRegistry)
+local OptionLocator = require(dir.Utility.OptionLocator)
 --#endregion required
 --[[
 This runs the initialization process and returns the object + relevant connections.
@@ -10,57 +10,83 @@ This runs the initialization process and returns the object + relevant connectio
 local ObjectInitializer = {}
 ObjectInitializer.__index = ObjectInitializer
 
-function ObjectInitializer.new(controller)
+function ObjectInitializer.new(controllerType)
     local self = setmetatable({}, ObjectInitializer)
-    self.controller = controller
+    self.controllerType = controllerType
+    self.registeredControllers = {}
+    self.registeredPrefabs = OptionLocator.new({})
     return self
 end
+
+-- register the instance, not the require
+function ObjectInitializer:RegisterController(key, module)
+    self.registeredControllers[key] = module
+end
+
+function ObjectInitializer:RegisterPrefabs(key, path)
+    self.registeredPrefabs.options[key] = {["path"] = path}
+end
+
+function ObjectInitializer:GetControllerInstance(root, required)
+    local controller, prefab
+    if not root:IsA("ModuleScript") then
+        warn(`root of {required.Parent.Name} should be a ModuleScript!`)
+        return
+    end
+    local config = require(root)
+
+    -- find prefab
+    -- TODO: what is this bro???? 0Head
+    self.registeredPrefabs:Select({[config.SystemType] = config.Prefab})
+    prefab = self.registeredPrefabs:Get(config.SystemType)
+
+    if not prefab then
+        warn(`prefab {config.Prefab} not found for system {config.SystemType}`)
+        prefab = {}
+    end
+
+    -- find controller (optional)
+    if config[self.controllerType] then
+        controller = self.registeredControllers[config[self.controllerType]]
+    end
+    
+    if not controller and config[self.controllerType] then
+        warn(`no controller for {required.Parent.Name}`)
+        return nil
+    end
+
+    -- gen and setup instance
+    local obj = require(controller).new(prefab, required)
+    obj.ClassName = controller.Name
+
+    return obj
+end
+
 ---initializes and registers obj. according to prefab/controller link
 ---@param required Folder the folder containing all components and references
 ---@return table the object and the corresponding method to destroy it
 function ObjectInitializer:Execute(required)
     if ObjectRegistry:WasRegistered(required) then
-        validator:Log("Object of GUID " .. dir.NetUtils:GetId(required) .. " was already registered.")
+        print(`Object of GUID {dir.NetUtils:GetId(required)} was already registered.`)
         return
     end
-    local entryPoint = validator:Exists(
-        required:FindFirstChild("InitRoot"), "InitRoot of activator")
+    local entryPoint = required:FindFirstChild("InitRoot")
+    local obj = self:GetControllerInstance(entryPoint, required)
+    if not obj then return end
 
-    local controllerRef = entryPoint:FindFirstChild(self.controller)
+    ------------ (the point where stuff actually happens) -------------
 
-    if not controllerRef then return end
-    if controllerRef.Value then
-        assert(controllerRef.Value.Parent, "controller of name " .. controllerRef.Value.Name .. "doesn't exist")
 
-        local controller = require(controllerRef.Value)
-        local prefabObj = entryPoint:FindFirstChild("Prefab")
-        local prefabVal = prefabObj and prefabObj.Value or nil
-        local prefab
-        if prefabObj and not prefabVal then
-            validator:Warn(string.format("prefab value doesn't exist for obj %s?", required.Parent.Name))
-            prefab = {}
-        end
-        prefab = prefabVal and require(entryPoint:FindFirstChild("Prefab").Value) or {}
-
-        validator:Exists(controller["new"], "new function of obj. controller")
-        local obj = controller.new(prefab, required)
-
-        -- assign className for debugging
-        obj.ClassName = controllerRef.Value.Name
-
-        -- attach a bunch of GC stuff, since this is a huge risk for memory leaks
-        local function DestroyObj()
-            ObjectRegistry:Deregister(required)
-        end
-        required.Destroying:Connect(DestroyObj)
-        return {
-            object = ObjectRegistry:Register(obj, required),
-            destroy = DestroyObj,
-        }
-    else
-       warn(`you are missing an assigned value in the {controllerRef.Name} objectvalue for {required.Parent.Name}`)
+    -- attach a bunch of GC stuff, since this is a huge risk for memory leaks
+    local function DestroyObj()
+        ObjectRegistry:Deregister(required)
     end
-    return nil
+    required.Destroying:Connect(DestroyObj)
+
+    return {
+        object = ObjectRegistry:Register(obj, required),
+        destroy = DestroyObj,
+    }
 end
 
 return ObjectInitializer
